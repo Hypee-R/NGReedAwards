64153cea1328e98ca8bbbf0f2400b1502912cc2d
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-05-09 11:38:58 -0500

    Enviar mensaje de contacto

--- src/app/user/contacto/contacto.component.ts
+++ src/app/user/contacto/contacto.component.ts
@@ -4,6 +4,7 @@ import { ToastrService } from 'ngx-toastr';
 import { VariablesService } from 'src/app/services/variablesGL.service';
 import { ContactoService } from 'src/app/services/contacto.service';
 
+
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
@@ -16,57 +17,38 @@ export class ContactoComponent implements OnInit {
   contactoForm: FormGroup;
   submitted: boolean;
 
-  constructor(
-    private fb: FormBuilder,
-    private toastr: ToastrService,
-    private contactoService: ContactoService,
-  
-    private variablesGL: VariablesService,
+  contactomodel = {
+    name: '',
+    correo: '',
+    mensaje:''
+  }
 
+  constructor(
+    private contactoService: ContactoService,
+    private toastr: ToastrService,
 
   ) { }
 
   ngOnInit(): void {
-    this.initForm();
-  }
-
-  initForm(){
-    this.contactoForm = this.fb.group({
-      nombre: ['', [Validators.required]],
-      correo: ['', [Validators.required]],
-      mensaje: ['', [Validators.required]],
-    })
-  }
-
   
+  }
 
-   
-  saveDataNominacion(){
-      this.contactoService.addNominacion({
-        nombre: this.contactoForm.get('nombre').value,
-        correo: this.contactoForm.get('correo').value,
-        mensaje: this.contactoForm.get('mensaje').value,
-        uid: JSON.parse(localStorage.d).uid
-      });
-      this.variablesGL.endProcessNominacion.subscribe(endProcessNominacion => {
-        if(endProcessNominacion != '' && endProcessNominacion != null){
-          this.toastr.success('Conctacto creada con exito!!', 'Success');
-          this.submitted = false;
-          this.contactoForm.reset();
-          console.log("END PROCESS CREATE");
 
-          this.variablesGL.endProcessCargaCompleta.next(null);
-          this.variablesGL.endProcessNominacion.next(null);
-          this.fetchNominaciones.emit(true);
-        }else if(endProcessNominacion == ''){
-          this.toastr.error('Hubo un error al guardar la nominacion!', 'Error');
-          this.submitted = false;
-        }
-      });
+  async add() {
+    const { name, correo,mensaje } = this.contactomodel;
+    await   this.contactoService.addNominacion({
+      nombre: name,
+      correo: correo,
+      mensaje: mensaje,
+      uid: JSON.parse(localStorage.d).uid
+    });
+    this.toastr.success('Alguien Se pondra en contacto!', 'Success');
+  }
 
-    }
 
- 
 
-}
+
+  }
+
+
 
