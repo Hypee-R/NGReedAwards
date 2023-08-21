1baf5c812981353157b10ffa5c126b67cd5d4a60
Jose daniel <jose.dejesus@it-and-apps.mx>
2023-08-21 17:04:28 -0600

    FIX:if name current user

--- src/app/modules/reedevento/home/home.component.ts
+++ src/app/modules/reedevento/home/home.component.ts
@@ -8,8 +8,8 @@ import { ComponentFixtureAutoDetect } from '@angular/core/testing';
 import { map, Observable, shareReplay, timer } from 'rxjs';
 import { DatePipe } from '@angular/common'
 import Swal from 'sweetalert2';
-
-
+import { collection, collectionData, Firestore, } from "@angular/fire/firestore";
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -17,16 +17,16 @@ import Swal from 'sweetalert2';
 })
 export class HomeComponent implements OnInit {
   targetVip = ''
-  mesas:string[]=["1","2","3","4","5","6","7","8","9","10"]
-  mesasVIP: string[][] = [["A","B","C","D"], ["E","F","G","H"]]
-  mesasN: string[][] = [["I","J","K","L","M","N","O"],
-  ["P","Q","R","S","T","U","V","W","X","Y","Z","A1","B2","C3","D4","E5","F6","G7","H8"]]
-  styleOBJ = {'background': "RGB(217, 222, 224)"}
+  mesas: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+  mesasVIP: string[][] = [["A", "B", "C", "D"], ["E", "F", "G", "H"]]
+  mesasN: string[][] = [["I", "J", "K", "L", "M", "N", "O"],
+  ["P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A1", "B2", "C3", "D4", "E5", "F6", "G7", "H8"]]
+  styleOBJ = { 'background': "RGB(217, 222, 224)" }
   toggle = true;
   status = "Enable";
-  componetesSeleccionados:ElementRef[]=[];
+  componetesSeleccionados: ElementRef[] = [];
   // txt1="A partir de esta fila son lugares individuales, \n aunque tambien puedes adquirir si lo prefieres \n mesas completas";
-  txt2="De la mesa A a la D son venta mesas completas";
+  txt2 = "De la mesa A a la D son venta mesas completas";
   // txt3="Ahora al escoger tú lugar, también puedes escoger entre tu plato fuerte en la cena:";
   // txt4="1.- Short Rib, espuma de bernesesa, pure de papa al tartufo y textura de papa";
   // txt5="2.- Salmón glaseado, risotto de tomate ahumado, tierra de parmesano y tomate seco";
@@ -34,31 +34,41 @@ export class HomeComponent implements OnInit {
 
 
   @ViewChildren('MyRef') inputsArray: QueryList<ElementRef>
-  selectedColor='background-color:rgb(143, 191, 22)'
-  unselectedColor='background-color:rgb(0, 0, 0)&:hover:{background: rgb(211, 202, 26)}'
-  enableColor='background-color:rgb(255, 6, 6)'
-  disbledColor=''
-  defaultColor=''
-  styleClickOn=''
-  visibleSidebar2=false
+  selectedColor = 'background-color:rgb(143, 191, 22)'
+  unselectedColor = 'background-color:rgb(0, 0, 0)&:hover:{background: rgb(211, 202, 26)}'
+  enableColor = 'background-color:rgb(255, 6, 6)'
+  disbledColor = ''
+  defaultColor = ''
+  styleClickOn = ''
+  visibleSidebar2 = false
   displayBasic: boolean;
-  boletosSeleccionados:boleto[]=[]
-  cargando=false
-  lugares:any
-  lugaresDisponibles:boleto[]=[]
+  boletosSeleccionados: boleto[] = []
+  cargando = false
+  lugares: any
+  lugaresDisponibles: boleto[] = []
   clock: any;
   source = timer(0, 1000);
   end: any;
   now: Date;
-  diferencia:number;
-  mesa:boolean=true;
-  StatusCargaLugares:boolean=false;
-
+  diferencia: number;
+  mesa: boolean = true;
+  StatusCargaLugares: boolean = false;
+  userData: any;
   constructor(
-    private lugaresService:LugaresService,
-    public datepipe: DatePipe
+    private lugaresService: LugaresService,
+    public datepipe: DatePipe,
+    private afs: Firestore,
+    private toastr: ToastrService
   ) {
+    this.getUserData().subscribe(data => {
+      if (data) {
+        this.userData = data.filter(item => item.uid === JSON.parse(localStorage.d).uid);
+        console.log(this.userData[0])
 
+
+      }
+    },
+    );
     this.getLugares();
     this.clock = this.source.subscribe(t => {
       this.now = new Date();
@@ -74,82 +84,81 @@ export class HomeComponent implements OnInit {
 
   }
 
-  async getLugares(){
-     await this.lugaresService.getLugares().subscribe( (data) => {
+  async getLugares() {
+    await this.lugaresService.getLugares().subscribe((data) => {
       this.lugares = data
 
-        this.StatusCargaLugares=true;
+      this.StatusCargaLugares = true;
 
-      this.lugaresDisponibles=[]
-      for (let dato of data){
-        let lug:boleto  ={idLugar:dato['idLugar'],precio:dato['precio'],comprado:dato['comprado'],apartado:dato['apartado'],hora:dato['fecha']}
+      this.lugaresDisponibles = []
+      for (let dato of data) {
+        let lug: boleto = { idLugar: dato['idLugar'], precio: dato['precio'], comprado: dato['comprado'], apartado: dato['apartado'], hora: dato['fecha'] }
         this.lugaresDisponibles.push(lug)
       }
 
 
 
-    this.initLugares()
-    this.cargando=true
+      this.initLugares()
+      this.cargando = true
     }, err => {
 
     });
 
   }
-  initLugares(){
+  initLugares() {
 
-      let toArray = this.inputsArray.toArray()
-      for (let lugar of this.lugaresDisponibles){
-        if(lugar.hora){
-          let f=lugar.hora.toString()
-          let newDate = new Date(f);
-          this.diferencia =  (this.now.getTime()-newDate.getTime())/60000;
+
+    let toArray = this.inputsArray.toArray()
+    for (let lugar of this.lugaresDisponibles) {
+      if (lugar.hora) {
+        let f = lugar.hora.toString()
+        let newDate = new Date(f);
+        this.diferencia = (this.now.getTime() - newDate.getTime()) / 60000;
+      }
+      let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == lugar.idLugar)
+      if (lugar.apartado || lugar.comprado) {
+        if (lugar.comprado) {
+
+          ref?.nativeElement?.setAttribute('style', this.enableColor)
         }
-        let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == lugar.idLugar)
-        if(lugar.apartado||lugar.comprado){
-          if(lugar.comprado){
+        else {
 
-            ref?.nativeElement?.setAttribute('style', this.enableColor)
-          }
-          else{
-
-            if(lugar.apartado){
-              if(!lugar.comprado && this.diferencia>2)
-              {
-                this.cancelarApartado(lugar)
-                ref?.nativeElement?.setAttribute('style', this.enableColor)
-              }
+          if (lugar.apartado) {
+            if (!lugar.comprado && this.diferencia > 2) {
+              this.cancelarApartado(lugar)
               ref?.nativeElement?.setAttribute('style', this.enableColor)
             }
+            ref?.nativeElement?.setAttribute('style', this.enableColor)
           }
-
-        }
-        else{
-          ref?.nativeElement?.setAttribute('style', this.unselectedColor)
         }
 
       }
+      else {
+        ref?.nativeElement?.setAttribute('style', this.unselectedColor)
+      }
+
+    }
 
   }
 
-  selectedAsiento(item){
+  selectedAsiento(item) {
     console.log(item);
 
-    let disponiblidad= this.lugaresDisponibles.find(el=>el.idLugar==item)
-    if(!disponiblidad.comprado&&!disponiblidad.apartado)
-    {
+    let disponiblidad = this.lugaresDisponibles.find(el => el.idLugar == item)
+    if (!disponiblidad.comprado && !disponiblidad.apartado) {
 
       let toArray = this.inputsArray.toArray()
       let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == item)
-      let status=this.componetesSeleccionados.find(el=>el?.nativeElement?.id==ref?.nativeElement?.id)
+      let status = this.componetesSeleccionados.find(el => el?.nativeElement?.id == ref?.nativeElement?.id)
 
-      if(!status){
+      if (!status) {
         this.componetesSeleccionados.push(ref)
         ref.nativeElement.setAttribute('style', this.selectedColor)
 
       }
-      else{
+      else {
 
-        this.componetesSeleccionados=this.componetesSeleccionados.filter(item=>item?.nativeElement?.id!=ref?.nativeElement?.id)
+        this.componetesSeleccionados = this.componetesSeleccionados.filter(item => item?.nativeElement?.id != ref?.nativeElement?.id)
         ref.nativeElement.setAttribute('style', this.unselectedColor)
       }
     }
@@ -158,17 +167,19 @@ export class HomeComponent implements OnInit {
 
   }
 
-  cancelarApartado(boleto:boleto){
-      this.lugaresService.cancelarLugarAparatdo(boleto)
-    }
+  getUserData() {
+    const itemsCollection = collection(this.afs, 'usuarios');
+    return collectionData(itemsCollection);
+  }
 
-  realizarCompra(){
 
-    const currentUser = JSON.parse(localStorage.getItem('d')) ?? null
+  cancelarApartado(boleto: boleto) {
+    this.lugaresService.cancelarLugarAparatdo(boleto)
+  }
 
+  realizarCompra() {
     console.log(localStorage.d)
-    if(localStorage.d== undefined) {
-
+    if (localStorage.d == undefined) {
       Swal.fire({
         icon: 'error',
         title: 'Oops...',
@@ -179,111 +190,112 @@ export class HomeComponent implements OnInit {
       return
     }
 
-    if (!currentUser.providerData[0].displayName) {
-      return Swal.fire(
+    if (!this.userData[0].firstName) {
+      Swal.fire(
         'Importante',
         'Perfil de usuario, aún requiere información para poder comprar tus boletos',
         'info'
       )
+
+      return
     }
 
-    if(this.componetesSeleccionados.length>0){
+
+    if (this.componetesSeleccionados.length > 0) {
 
 
-      for(let boleto of this.componetesSeleccionados){
-        let newBoleto={"idLugar":boleto?.nativeElement?.id,"precio":"600USD","comprado":false,"apartado":false,"hora":this.now.toLocaleString('en-US')}
-        let estatus=this.lugaresService.getLugaresPagados(newBoleto)
+      for (let boleto of this.componetesSeleccionados) {
+        let newBoleto = { "idLugar": boleto?.nativeElement?.id, "precio": "600USD", "comprado": false, "apartado": false, "hora": this.now.toLocaleString('en-US') }
+        let estatus = this.lugaresService.getLugaresPagados(newBoleto)
 
 
       }
 
 
 
-      this.visibleSidebar2=true;
+      this.visibleSidebar2 = true;
       if (this.targetVip === 'VIP1') {
-        for(let boleto of this.componetesSeleccionados){
-          let newBoleto={"idLugar":boleto?.nativeElement?.id,"precio":"630USD","comprado":false,"apartado":false,"hora":this.now.toLocaleString('en-US')}
+        for (let boleto of this.componetesSeleccionados) {
+          let newBoleto = { "idLugar": boleto?.nativeElement?.id, "precio": "630USD", "comprado": false, "apartado": false, "hora": this.now.toLocaleString('en-US') }
           this.boletosSeleccionados.push(newBoleto)
         }
       } else if (this.targetVip === 'VIP2') {
-        for(let boleto of this.componetesSeleccionados){
-          let newBoleto={"idLugar":boleto?.nativeElement?.id,"precio":"620USD","comprado":false,"apartado":false,"hora":this.now.toLocaleString('en-US')}
+        for (let boleto of this.componetesSeleccionados) {
+          let newBoleto = { "idLugar": boleto?.nativeElement?.id, "precio": "620USD", "comprado": false, "apartado": false, "hora": this.now.toLocaleString('en-US') }
           this.boletosSeleccionados.push(newBoleto)
         }
       } else {
-        for(let boleto of this.componetesSeleccionados){
-          let newBoleto={"idLugar":boleto?.nativeElement?.id,"precio":"600USD","comprado":false,"apartado":false,"hora":this.now.toLocaleString('en-US')}
+        for (let boleto of this.componetesSeleccionados) {
+          let newBoleto = { "idLugar": boleto?.nativeElement?.id, "precio": "600USD", "comprado": false, "apartado": false, "hora": this.now.toLocaleString('en-US') }
           this.boletosSeleccionados.push(newBoleto)
         }
       }
       this.actualizarBoleto()
 
     }
-    else{
+    else {
       this.displayBasic = true;
     }
 
   }
 
-  unseled()
-  {
-    this.boletosSeleccionados=[]
-    this.componetesSeleccionados=[]
+  unseled() {
+    this.boletosSeleccionados = []
+    this.componetesSeleccionados = []
   }
-  cancelarCompra(){
+  cancelarCompra() {
 
-    for (let item of this.inputsArray){
+    for (let item of this.inputsArray) {
 
       item?.nativeElement?.setAttribute('style', this.unselectedColor)
     }
-    this.componetesSeleccionados=[]
-    this.boletosSeleccionados=[]
+    this.componetesSeleccionados = []
+    this.boletosSeleccionados = []
   }
 
-  actualizarBoleto(){
+  actualizarBoleto() {
 
-      this.lugaresService.updatelugarApartado(this.boletosSeleccionados)
+    this.lugaresService.updatelugarApartado(this.boletosSeleccionados)
 
 
   }
 
-  comprarMesa(idMesa, target: string){
+  comprarMesa(idMesa, target: string) {
     ///console.log(idMesa+"0")
-    let disponiblidad= this.lugaresDisponibles.find(el=>el.idLugar==idMesa+"1")
-    if(!disponiblidad.comprado&&!disponiblidad.apartado)
-    {
+    let disponiblidad = this.lugaresDisponibles.find(el => el.idLugar == idMesa + "1")
+    if (!disponiblidad.comprado && !disponiblidad.apartado) {
       let toArray = this.inputsArray.toArray()
-      let colorMesa=false
-      for (let silla of this.mesas){
-        let item=idMesa+silla
+      let colorMesa = false
+      for (let silla of this.mesas) {
+        let item = idMesa + silla
         let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == item)
-        let status=this.componetesSeleccionados.find(el=>el?.nativeElement?.id==ref?.nativeElement?.id)
-        if(!status){
-        this.componetesSeleccionados.push(ref)
+        let status = this.componetesSeleccionados.find(el => el?.nativeElement?.id == ref?.nativeElement?.id)
+        if (!status) {
+          this.componetesSeleccionados.push(ref)
+          ref.nativeElement.setAttribute('style', this.selectedColor)
+          colorMesa = true
+          this.targetVip = target
+        }
+        else {
+          this.componetesSeleccionados = this.componetesSeleccionados.filter(item => item?.nativeElement?.id != ref?.nativeElement?.id)
+          ref.nativeElement.setAttribute('style', this.unselectedColor)
+          colorMesa = false
+        }
+
+      }
+      let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == idMesa)
+      if (colorMesa) {
         ref.nativeElement.setAttribute('style', this.selectedColor)
-        colorMesa=true
-        this.targetVip = target
       }
-      else{
-        this.componetesSeleccionados=this.componetesSeleccionados.filter(item=>item?.nativeElement?.id!=ref?.nativeElement?.id)
+      else {
         ref.nativeElement.setAttribute('style', this.unselectedColor)
-        colorMesa=false
       }
 
     }
-    let ref: ElementRef<HTMLInputElement> = toArray.find(el => el?.nativeElement?.id == idMesa)
-    if(colorMesa){
-      ref.nativeElement.setAttribute('style', this.selectedColor)
-    }
-    else{
-      ref.nativeElement.setAttribute('style', this.unselectedColor)
-    }
-
-  }
   }
   showBasicDialog() {
     this.displayBasic = true;
-}
+  }
 
 
 }
