ab0920bc91d2404241f7390a987448925e3eef3e
Jose daniel <jose.dejesus@it-and-apps.mx>
2024-06-10 14:15:13 -0600

    mejora de paises json en ligar del servicio y costo

--- src/app/modules/user/mis-nominaciones/add-nominacion/add-nominacion.component.ts
+++ src/app/modules/user/mis-nominaciones/add-nominacion/add-nominacion.component.ts
@@ -26,7 +26,7 @@ export class AddNominacionComponent implements OnInit, OnDestroy {
   @Output() fetchNominaciones: EventEmitter<boolean> = new EventEmitter<boolean>()
    producto = {
      descripcion : 'Compra por Nominacion de Reedlatino',
-     precio      :  135.00
+     precio      :  152.00
    }
 
   codigosDescuento = [
--- src/app/services/paises.service.ts
+++ src/app/services/paises.service.ts
@@ -1,19 +1,31 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  url: string = 'https://restcountries.com/v3.1/all';
-  constructor(
-    private http: HttpClient,
-  ){
+  // url: string = 'https://restcountries.com/v3.1/all';
+  // constructor(
+  //   private http: HttpClient,
+  // ){
 
-  }
+  // }
 
-  getPaises(){
+  // getPaises(){
+  //   return this.http.get(this.url);
+  // }
+
+  // URL del archivo JSON local
+  private url: string = 'assets/utils/Paises.json';
+
+  constructor(private http: HttpClient) { }
+
+  // Método para obtener los datos del JSON local
+  getPaises(): Observable<any> {
     return this.http.get(this.url);
   }
+
 }
